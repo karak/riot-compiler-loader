08c0e2c044b52f4191a2cf9b3997d9407c6890e7
karak <karak97@gmail.com>
2019-01-09 17:27:09 +0900

    feat: create initial loader

---
+++ index.js
@@ -0,0 +1,26 @@
+const { compile } = require("@riotjs/compiler");
+const { getOptions } = require("loader-utils");
+
+module.exports = function(source) {
+  // parse the user query
+  const query = getOptions(this) || {};
+
+  // normalise the query object in case of question marks
+  const opts = Object.keys(query).reduce(function(acc, key) {
+    acc[key.replace("?", "")] = query[key];
+    return acc;
+  }, {});
+
+  // cache this module
+  if (this.cacheable) this.cacheable();
+
+  // compile to generate entities
+  const callback = this.async();
+  compile(source, opts)
+    .then(({ code, map }) => {
+      callback(null, code, map);
+    })
+    .catch(err => {
+      callback(err);
+    });
+};
